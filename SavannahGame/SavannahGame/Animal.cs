1e4dc9729c4463647200474b4b5ba410a1fb580b
MartinDKnudsen <66463206+MartinDKnudsen@users.noreply.github.com>
2020-09-04 11:13:47 +0200

    Added new classes

---
+++ SavannahGame/SavannahGame/Animal.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace SavannahGame
+{
+    abstract class Animal
+    {
+
+        string Genger;
+
+        int Number;
+
+        double Weight;
+        double WeigthGain;
+        double WeightLoss;
+
+        int xCon;
+        int yCon;
+
+
+        List<Animal> Animals;
+
+        public abstract void Eat();
+
+        public abstract void Move();
+
+
+
+    }
+}
---
+++ SavannahGame/SavannahGame/BoardFields.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SavannahGame
+{
+    public class BoardFields
+    {
+        //Number af fields generated in two dim Array
+
+        public int[,] GenerateFields()
+        {
+
+         return new int[21, 21;
+
+        }
+
+    }
+}

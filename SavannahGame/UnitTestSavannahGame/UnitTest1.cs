ef1e290dd144bd8be4b72920a2afc6eebc8b2e28
MartinDKnudsen <martinknu22@gmail.com>
2020-10-08 16:00:28 +0200

    UnitTestAttemp

---
+++ SavannahGame/UnitTestSavannahGame/UnitTest1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using BusinessLogic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestSavannahGame
+{
+    [TestClass]
+    public class UnitTest1
+    {
+    
+        [TestMethod]
+        public void Check_If_400_Fields_Is_Created_In_GameLogic()
+        {
+
+            GameLogic gl = GameLogic.Getinstance();
+            CountData ct = CountData.Getinstance();
+
+            gl.StartGame(10, 10, 10);
+
+
+            int expected = 400;
+            int methodToTest = ct.CountFields();
+
+            Assert.AreEqual(expected, methodToTest);
+
+
+        }
+    }
+}
